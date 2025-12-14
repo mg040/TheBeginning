202fd824ab863ad036ed1b2fb7ac3aeb784a76cc
Martin Götz <e12318533@student.tuwien.ac.at>
2025-12-14 10:47:04 +0100

    Add ItemsPackeage [empty]

---
+++ src/main/java/net/M/examplemod/item/Moditems.java
@@ -0,0 +1,16 @@
+package net.M.examplemod.item;
+
+import net.M.examplemod.TutorialMod;
+import net.minecraft.world.item.Item;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+
+public class Moditems {
+
+    public static final DeferredRegister<Item> ITEMS =
+        DeferredRegister.create(ForgeRegistries.ITEMS, TutorialMod.MOD_ID);
+
+    
+
+
+}
